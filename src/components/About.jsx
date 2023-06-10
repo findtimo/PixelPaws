f2e988fff84596e35a309ee81293505c83e89c7a
tkyj <82047680+tkyj@users.noreply.github.com>
2023-06-10 21:41:43 +0800

    Updated About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,17 @@
 
-import { Box, Button, Flex, Image, Link, Spacer, Text, Input } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 
 const About = ({}) => {
 
     return (
-        <div>
-      <Flex justify="center" align="center" height="100vh" paddingBottom="30rem" overflow="auto">
-        <Box width="70vw" height="100%">
-            <div>
+      <Flex justify="center" align="center" height="75vh" width="100vw">
+        <Box mr="15vw" ml="15vw" overflow="scroll" height="100%" width="100vw">
+            <>
                 <h1>
-                    About Pixel Paws
+                    About PixelPaws
                 </h1>
-                <Text fontSize="1.4em" letterSpacing="-5.5%" fontFamily="VT323" textShadow="0 2px 2px #000000">
+                <br />
+                <Text fontSize="1.4em" fontFamily="VT323" textShadow="0 2px 2px #000000">
                     An NFT initiative project by Timothy Koh. The website is built using Vite and runs ReactJS and Typescript.
                     PixelPaws was created using ERC-721 standard for non-fungible tokens on the Ethereum blockchain. 
                 </Text>
@@ -19,7 +19,7 @@ const About = ({}) => {
                 <h2>
                     NFT Art Creation
                 </h2>
-                <Text fontSize="1.4em" letterSpacing="-5.5%" fontFamily="VT323" textShadow="0 2px 2px #000000">
+                <Text fontSize="1.4em" fontFamily="VT323" textShadow="0 2px 2px #000000">
                     To begin, individal layers for the artwork is created and hand-drawn using Photoshop. The original idea was created using an AI text-to-image generation software Dall-E.
                     {/* <img src='' alt='Draft mockup of PixelPaws'/> */}
                     It is drafted up and later exported to PNG using HashLip's NFT generator which randomizes and generates the final artwork using weights assigned to create scarcity and rare attributes.
@@ -30,7 +30,7 @@ const About = ({}) => {
                 <h2>
                     Writing Contract
                 </h2>
-                <Text fontSize="1.4em" letterSpacing="-5.5%" fontFamily="VT323" textShadow="0 2px 2px #000000">
+                <Text fontSize="1.4em" fontFamily="VT323" textShadow="0 2px 2px #000000">
                     The next step was to write the Solidity contract. Tech used were Hardhat for testing, Ethers for interacting with smart contracts through the browser, as well as OpenZeppelin to generate the template for the industry standard smart contract (https://www.openzeppelin.com/).
                     The main functions that had to be coded were: mint, isContentOwned, as well as function checkers. To keep the gas fees cheap, it was important to ensure that each line of code was compact and essential.
                 </Text>
@@ -38,7 +38,7 @@ const About = ({}) => {
                 <h2>
                     Linking to Website
                 </h2>
-                <Text fontSize="1.4em" letterSpacing="-5.5%" fontFamily="VT323" textShadow="0 2px 2px #000000">
+                <Text fontSize="1.4em" fontFamily="VT323" textShadow="0 2px 2px #000000">
                     After the .sol was created, comes linking it the website using Ethers import.
                     Equal amount of time as the contract writing was spent here as conditional statements and error checkings before sending the mint over to Ether to minimise error during on the contract level.
                     This included priced to paid calculations, quantity, etc.
@@ -46,16 +46,25 @@ const About = ({}) => {
                 </Text>
                 <br />
                 <h2>
+                    Final Steps
+                </h2>
+                <Text fontSize="1.4em" fontFamily="VT323" textShadow="0 2px 2px #000000">
+                    The last step was to deploy it to an actual network such as Polygon which offers free testing, instead of hosting the ledger locally.
+                    This allowed me to publish it to Opensea's test net which can be viewed
+                    <a href='https://testnets.opensea.io/collection/pixelpaws' target='_blank'> here on Opensea! </a>
+                    This also allows me to pull NFT's associated with the owner from the meta-data in the token that links to a Pinata image repository.
+                </Text>
+                <br />
+                <h2>
                     Experience
                 </h2>
-                <Text fontSize="1.4em" letterSpacing="-5.5%" fontFamily="VT323" textShadow="0 2px 2px #000000">
+                <Text fontSize="1.4em" fontFamily="VT323" textShadow="0 2px 2px #000000">
                     Overall, this project was done to expose me to the inner workings of BlockChain, and understand how contracts work fundamentally. Along the way I also learnt about testing concepts before uploading to a proper network and market like OpenSea.
                 </Text>
                 <br />
-            </div>
+            </>
             </Box>
         </Flex>
-        </div>
     );
 }
 
